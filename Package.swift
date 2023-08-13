d83dc20a2fc3aa794b229d5b86725dffb520e2a1
5HT <maxim@synrc.com>
2023-08-13 16:08:35 +0300

    cert support

--- Package.swift
+++ Package.swift
@@ -5,24 +5,12 @@ import class Foundation.ProcessInfo
 
 let package = Package(
     name: "chat-x509",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13)
-    ],
-    products: [
-        .executable(name: "chat-x509", targets: ["swift-console"]),
-    ],
-    targets: [
-        .executableTarget(
-            name: "swift-console",
-            dependencies: [
-                .product(name: "X509", package: "swift-certificates")
-            ]),
-    ]
+    platforms: [ .macOS(.v10_15), .iOS(.v13) ],
+    products: [ .executable(name: "chat-x509", targets: ["swift-console"]), ],
+    targets: [ .executableTarget(name: "swift-console",
+       dependencies: [ .product(name: "X509", package: "swift-certificates"), ]), ]
 )
 
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
-    package.dependencies += [
-        .package(url: "https://github.com/apple/swift-certificates.git", branch: "main"),
-    ]
+    package.dependencies += [ .package(url: "https://github.com/apple/swift-certificates", branch: "main"), ]
 }
--- Sources/swift-console/main.swift
+++ Sources/swift-console/main.swift
@@ -1,14 +1,24 @@
-import Darwin.libc
 import Foundation
-import X509
+@testable @_spi(CMS) import X509
 
 func showCMS(name: String) throws {
   print("CMS: \(name)")
   let url = URL(fileURLWithPath: name)
   do {
      let data = try Data(contentsOf: url)
-//     let cms = try CMSContentInfo(derEncoded: Array(data))
-     print("\(data)")
+     let cms = try CMSContentInfo(derEncoded: Array(data))
+//     let env = try CMSEnvelopenContentInfo(derEncoded: Array(cms.content))
+     print("\(cms.contentType)")
+  }
+}
+
+func showCRT(name: String) throws {
+  print("Cert: \(name)")
+  let url = URL(fileURLWithPath: name)
+  do {
+     let data = try Data(contentsOf: url)
+     let crt = try Certificate(derEncoded: Array(data))
+     print("\(crt)")
   }
 }
 
@@ -18,7 +28,7 @@ func showCSR(name: String) throws {
   do {
      let data = try Data(contentsOf: url)
      let csr = try CertificateSigningRequest(derEncoded: Array(data))
-     print("\(csr.signatureAlgorithm)")
+     print("\(csr)")
   }
 }
 
@@ -30,6 +40,7 @@ func run() throws {
         if (data.count > 2 && data[0] == "show") {
            print("Show: \(data)")
            switch (data[2]) {
+              case "crt": try showCRT(name: data[1])
               case "csr": try showCSR(name: data[1])
               case "cms": try showCMS(name: data[1])
               default: ()
