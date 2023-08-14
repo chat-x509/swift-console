5ab329465e752bb56a58dcaf6ab23f1abfcebe3a
5HT <maxim@synrc.com>
2023-08-14 13:35:25 +0300

    kdf

--- Sources/SwiftConsole/kdf.swift
+++ Sources/SwiftConsole/kdf.swift
@@ -4,6 +4,11 @@ class KDF {
 
   public static func derive(hash: String, key: Data, len: Int, data: Data) throws -> Data {
       let data = Data(count: len)
+
+      for i in 0...len {
+
+      }
+
       return data
   }
 
