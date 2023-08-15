34c2ce004fe51e87d2287834948b249895a8495f
5HT <maxim@synrc.com>
2023-08-15 21:06:38 +0300

    form

--- Sources/SwiftConsole/form.swift
+++ Sources/SwiftConsole/form.swift
@@ -21,15 +21,25 @@ public class Form {
   }
 
   public static func show(data: Array<String>) {
+    var settings:termios = termios()
+    tcgetattr(STDIN_FILENO, &settings)
+    var saved = settings
+    settings.c_lflag &= ~(UInt32(ICANON) | UInt32(ECHO))
+    tcsetattr(STDIN_FILENO, TCSANOW, &settings)
     for i in 0...app.count-1 {
-      print("\u{27}[0m\u{27}[1;97m\u{27}[45m    \u{27}[0;97m\u{27}[1;104m \(app[i].name)\n\u{27}[0m\u{27}[0K")
+      print("\u{1b}[0m\u{1b}[1;97m\u{1b}[45m    \u{1b}[0;97m\u{1b}[1;104m \(app[i].name)\u{1b}[0m\u{1b}[0K")
     }
+    for i in 0...app.count-1 {
+      if (app[i].name == Console.no) {
+         for j in 0...app[i].sections[0].rows.count-1 {
+            print("\u{1b}[0m\u{1b}[1;97m\u{1b}[45m \(app[i].sections[0].rows[j].no) ", terminator: "")
+            print("\u{1b}[0;93m\u{1b}[0;104m \(app[i].sections[0].rows[j].desc)", terminator: "")
+            print("\u{1b}[0m\u{1b}[0K")
+         }
+      }
+    }
+    tcsetattr(STDIN_FILENO, TCSANOW, &saved)
   }
-
-
-//    :lists.map(fn CHAT.row(no: no, desc: desc, rico: _rico) ->
-//        :io.format '\e[0m\e[1;97m\e[45m ~2.. B \e[0;93m\e[0;104m ~40.. ts ~n\e[0m\e[0K', [no,desc]
-//  end, rows)
 
   public static var app: [screen] = [
     screen(
