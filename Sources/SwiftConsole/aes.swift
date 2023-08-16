e182792bea1fd1327b8a18b9c5fa5cae1d9157d4
5HT <maxim@synrc.com>
2023-08-17 02:20:27 +0300

    app x-forms

--- Sources/SwiftConsole/aes.swift
+++ Sources/SwiftConsole/aes.swift
@@ -12,8 +12,9 @@ extension Data {
         var bytes = [UInt8]()
         var index = hex.startIndex
         while index < hex.endIndex {
-            let byteString = hex[index..<hex.index(index, offsetBy: 2)]
-            if let byte = UInt8(byteString, radix: 16) { bytes.append(byte) } else { return nil }
+            let string = hex[index..<hex.index(index, offsetBy: 2)]
+            if let byte = UInt8(string, radix: 16)
+                 { bytes.append(byte) } else { return nil }
             index = hex.index(index, offsetBy: 2)
         }
         self.init(bytes)
@@ -24,19 +25,19 @@ public class Block {
     public static var IV: UInt64 = 0xA6A6A6A6A6A6A6A6
     public static var vector: [wrapVector] = [
        wrapVector(
-            kek: SymmetricKey(data: Data(hex: "f59782f1dceb0544a8da06b34969b9212b55ce6dcbdd0975a33f4b3f88b538da")!),
-            key: SymmetricKey(data: Data(hex: "73d33060b5f9f2eb5785c0703ddfa704")!),
+           kek: SymmetricKey(data: Data(hex: "f59782f1dceb0544a8da06b34969b9212b55ce6dcbdd0975a33f4b3f88b538da")!),
+           key: SymmetricKey(data: Data(hex: "73d33060b5f9f2eb5785c0703ddfa704")!),
            wrap: Data(hex: "2e63946ea3c090902fa1558375fdb2907742ac74e39403fc")!), ]
     public static func testKeyWrap() throws {
        try vector.forEach { e in
-            let kek = SymmetricKey(data: e.kek)
-            let wrapped = try AES.KeyWrap.wrap(e.key, using: kek) // = e.wrap
-            let unwrapped = try AES.KeyWrap.unwrap(wrapped, using: kek) // e.key
-            print(": KEK \(e.kek)")
-            print(": KEY \(e.key)")
-            print(": WRAP \(Array(e.wrap))")
-            print(": Wrapped \(Array(wrapped))")
-            print(": Unwrapped \(unwrapped)")
+           let kek = SymmetricKey(data: e.kek)
+           let wrapped = try AES.KeyWrap.wrap(e.key, using: kek) // = e.wrap
+           let unwrapped = try AES.KeyWrap.unwrap(wrapped, using: kek) // e.key
+           print(": KEK \(e.kek)")
+           print(": KEY \(e.key)")
+           print(": WRAP \(Array(e.wrap))")
+           print(": Wrapped \(Array(wrapped))")
+           print(": Unwrapped \(unwrapped)")
        }
     }
 
