95c27e511dee6e7dc762f9f57ce06cfe5ea0864e
5HT <maxim@synrc.com>
2023-08-17 01:46:24 +0300

    swift code generation FT compiler for iOS

--- Sources/SwiftConsole/form.swift
+++ Sources/SwiftConsole/form.swift
@@ -22,25 +22,28 @@ public class Form {
   public struct row {
     let no: Int
     let desc: String
-    let lico: (String,String)
     let rico: (String,String)
   }
 
   public static func show(data: Array<String>) throws {
-    let app = App.screens
-    for i in 0...app.count-1 {
-      if (app[i].no == Console.no) {
-         let screen = app[i].name
-         let name = app[i].sections[0].name
-         print("     \u{1b}[0;97m\u{1b}[1;104m \(screen) \u{1b}[0m\u{1b}[0K")
-         print("\u{1b}[1;33m\u{1b}[44m▄▄▄▄▄\u{1b}[0;97m\u{1b}[1;104m \(name) \u{1b}[0m\u{1b}[0K")
-         for j in 0...app[i].sections[0].rows.count-1 {
+    if (data.count < 2) {
+      print(": Not enough arguments.")
+    } else {
+      let app = App.screens
+      for i in 0...app.count-1 {
+        if (app[i].no == Int(data[1])) {
+          let screen = app[i].name
+          let name = app[i].sections[0].name
+          print("     \u{1b}[0;97m\u{1b}[1;104m \(screen) \u{1b}[0m\u{1b}[0K")
+          print("\u{1b}[1;33m\u{1b}[44m▄▄▄▄▄\u{1b}[0;97m\u{1b}[1;104m \(name) \u{1b}[0m\u{1b}[0K")
+          for j in 0...app[i].sections[0].rows.count-1 {
             let no = String(app[i].sections[0].rows[j].no).leftPad(toLength: 3, withPad: "0")
             let desc = String(app[i].sections[0].rows[j].desc).padding(toLength: 30, withPad: " ", startingAt:0)
             print("\u{1b}[0m\u{1b}[1;97m\u{1b}[45m \(no) ", terminator: "")
             print("\u{1b}[0;93m\u{1b}[0;104m \(desc)", terminator: "")
             print("\u{1b}[0m\u{1b}[0K")
-         }
+          }
+        }
       }
     }
   }
